b4f8b918b4f3c2b62e94455155eaec8bdc2704df
Mzrbt <jabetmarin07@gmail.com>
2026-03-16 10:00:38 +0100

    Advancing LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,34 @@
-<script>
+<script setup>
+
+    import { ref, computed } from 'vue'
+    import { useRouter } from 'vue-router'
+    import { login } from '../composables/useApi'
+
+    const router = useRouter()
+
+    const email = ref("")
+    const password = ref("")
+
+    const isFormValid = computed(() => {
+        return email.value.trim() && password.value.trim()
+    })
+
+    const isLoading = ref(false)
+
+    async function handleLogin() {
+
+        isLoading.value = true
+        
+        try {
+            const data = await login(email.value, password.value)
+            localStorage.setItem('token', data.token)
+            router.push('/ranking')
+        } catch (e) {
+            console.log('Identifiants incorrects')
+        } finally {
+            isLoading.value = false
+        }
+    }
 
 </script>
 
